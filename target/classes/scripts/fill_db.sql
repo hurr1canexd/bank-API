2c8c13c3bb748444292235d242ae3f86f79052d5
Слепцов Михаил Александрович <MASleptsov@sberbank.ru>
2021-05-20 14:32:14 +0300

    removing all files in .gitignore

--- target/classes/scripts/fill_db.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO CLIENT (firstname, lastname)
-VALUES ( 'Ivan', 'Ivanov' );
-
-INSERT INTO ACCOUNT (number, balance, clientid)
-VALUES ( '384979256485796247816', 0, 1 );
-
-INSERT INTO CARD (number, month, year, code, accountid)
-VALUES ( '42024305346286324576', '07', '2028', 228, 1);
